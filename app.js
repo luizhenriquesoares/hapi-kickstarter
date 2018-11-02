66426a3b556f3ae5f806a3f9d416a8691a5c4cfb
luiz henrique soares <luizhenriquesoares91@gmail.com>
2018-11-01 21:30:21 -0300

    add first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+const init = async () => {
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+
+        return 'Hello, world!';
+    }
+});
+
+init();
